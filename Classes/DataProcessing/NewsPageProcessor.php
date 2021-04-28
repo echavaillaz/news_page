6e120b9459d661bfd8663bfdd4a643c6fa2b3f04
Eric Chavaillaz <eric.chavaillaz@gmail.com>
2021-04-28 17:21:45 +0200

    [TASK] Use constructor injection in data processor

--- Classes/DataProcessing/NewsPageProcessor.php
+++ Classes/DataProcessing/NewsPageProcessor.php
@@ -20,28 +20,36 @@ namespace Pint\NewsPage\DataProcessing;
 use Pint\NewsPage\Domain\Model\NewsPage;
 use Pint\NewsPage\Domain\Repository\NewsPageRepository;
 use Pint\NewsPage\Domain\Repository\PageRepository;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
 
 class NewsPageProcessor implements DataProcessorInterface
 {
+    protected NewsPageRepository $newsPageRepository;
+
+    public function __construct(NewsPageRepository $newsPageRepository)
+    {
+        $this->newsPageRepository = $newsPageRepository;
+    }
+
     public function process(
         ContentObjectRenderer $cObj,
         array $contentObjectConfiguration,
         array $processorConfiguration,
         array $processedData
     ): array {
-        if ($processedData['data']['doktype'] === PageRepository::DOKTYPE_NEWS) {
-            $processedData['newsItem'] = $this->getNewsItem($this->getPageId($processedData['data']));
+        if ($processedData['data']['doktype'] !== PageRepository::DOKTYPE_NEWS) {
+            return $processedData;
         }
+
+        $processedData['newsItem'] = $this->getNewsItem($this->getPageId($processedData['data']));
 
         return $processedData;
     }
 
     protected function getNewsItem(int $pageId): ?NewsPage
     {
-        return GeneralUtility::makeInstance(NewsPageRepository::class)->findOneByPageId($pageId);
+        return $this->newsPageRepository->findOneByPageId($pageId);
     }
 
     protected function getPageId(array $pageRecord): int
