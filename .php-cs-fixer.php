879298aa5895636f209a5a6ae19e9454dedb04e0
Eric Chavaillaz <eric@hemmer.ch>
2021-05-04 09:16:42 +0200

    [TASK] Update PHP-CS-Fixer to 3.0

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -32,7 +32,7 @@ return (new Config())
         'no_blank_lines_after_phpdoc' => true,
         'no_empty_phpdoc' => true,
         'no_empty_statement' => true,
-        'no_extra_consecutive_blank_lines' => true,
+        'no_extra_blank_lines' => true,
         'no_leading_namespace_whitespace' => true,
         'no_null_property_initialization' => true,
         'no_short_bool_cast' => true,
